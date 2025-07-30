6ebc55781cc4d2990ab7b3ce579cdc21291e3d1b
Mathis Logemann <13556116+mathisloge@users.noreply.github.com>
2025-07-30 07:16:23 +0200

    feat(drv8825): add test program

---
+++ utils/drv8825_test/main.cpp
@@ -0,0 +1,32 @@
+#include <thread>
+#include <boost/asio.hpp>
+#include <cm/hw/drv8825_stepper_moter.hpp>
+#include <fmt/core.h>
+
+using namespace mp_units::si::unit_symbols;
+
+int main()
+{
+    boost::asio::io_context io;
+    cm::Drv8825StepperMotorDriver stepper{cm::Drv8825EnablePin{.chip = "/dev/gpiochip0", .offset = {24}},
+                                          cm::Drv8825StepPin{.chip = "/dev/gpiochip0", .offset = {23}},
+                                          cm::Drv8825DirectionPin{.chip = "/dev/gpiochip0", .offset = {23}}};
+
+    boost::asio::co_spawn(
+        io,
+        [&stepper] -> boost::asio::awaitable<void> {
+            boost::asio::steady_timer timer{co_await boost::asio::this_coro::executor};
+
+            fmt::println("start");
+            timer.expires_after(std::chrono::seconds{1});
+            co_await timer.async_wait(boost::asio::use_awaitable);
+
+            co_await stepper.step(1000, (2 * cm::step) / s);
+
+            co_return;
+        },
+        boost::asio::detached);
+
+    io.run();
+    return 0;
+}

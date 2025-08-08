d03eeff639fba4adc4e91b70a06afb05320eda9b
Mathis Logemann <13556116+mathisloge@users.noreply.github.com>
2025-08-08 13:56:38 +0200

    build: remove fmt overlay as it is now 11.2.0 in main list

--- cmake/overlays/qtbase/portfile.cmake
+++
@@ -1,6 +0,0 @@
-# SPDX-FileCopyrightText: 2025 Mathis Logemann <mathis@quite.rocks>
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
-# SPDX-License-Identifier: MIT
-
-set(VCPKG_POLICY_EMPTY_PACKAGE enabled)

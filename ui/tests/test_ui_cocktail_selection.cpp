84bec1c3ace17fb75906eed79c3345dd2f7ae3ba
Mathis Logemann <13556116+mathisloge@users.noreply.github.com>
2025-08-24 18:10:27 +0200

    docs: add missing license headers

--- ui/tests/test_ui_cocktail_selection.cpp
+++ ui/tests/test_ui_cocktail_selection.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Mathis Logemann <mathis@quite.rocks>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include <catch2/catch_test_macros.hpp>
 #include <quite/test/probe.hpp>
 #include <quite/test/probe_manager.hpp>

14c8767307ebf687ab9d32b39c5ee5219c683df4
Mathis Logemann <13556116+mathisloge@users.noreply.github.com>
2025-10-23 20:44:48 +0200

    feat: export initial set of translation texts

--- i18n/cocktail-maker_en.ts
+++ i18n/cocktail-maker_en.ts
@@ -1,6 +1,104 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!--
-Run the update_translations CMake target to populate the source strings in this file.
--->
 <!DOCTYPE TS>
-<TS version="2.1" language="en" sourcelanguage="de"/>
+<TS version="2.1" language="en" sourcelanguage="de">
+<context>
+    <name>GlassSelectionPage</name>
+    <message>
+        <location filename="../ui/app/GlassSelectionPage.qml" line="42"/>
+        <source>Glasgöße wählen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/app/GlassSelectionPage.qml" line="49"/>
+        <source>Erkannt: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <location filename="../ui/app/Main.qml" line="15"/>
+        <source>🍹 Cocktail Automat</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MixPage</name>
+    <message>
+        <location filename="../ui/app/MixPage.qml" line="21"/>
+        <source>%1 wird zubereitet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/app/MixPage.qml" line="35"/>
+        <source>Schritte:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/app/MixPage.qml" line="104"/>
+        <source>Abbrechen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/app/MixPage.qml" line="113"/>
+        <source>Bitte führe den angezeigten Schritt aus:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/app/MixPage.qml" line="115"/>
+        <source>Fertig ▶</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/app/MixPage.qml" line="126"/>
+        <source>Bitte fülle nach:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/app/MixPage.qml" line="128"/>
+        <source>Aufgefüllt ▶</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RecipeDetailPage</name>
+    <message>
+        <location filename="../ui/app/RecipeDetailPage.qml" line="37"/>
+        <source>Zurück</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/app/RecipeDetailPage.qml" line="69"/>
+        <source>Getränk boosten</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/app/RecipeDetailPage.qml" line="81"/>
+        <source>mild</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/app/RecipeDetailPage.qml" line="95"/>
+        <source>stark</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/app/RecipeDetailPage.qml" line="109"/>
+        <source>Schritte:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/app/RecipeDetailPage.qml" line="141"/>
+        <source>🍹 mixen!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RecipeListPage</name>
+    <message>
+        <location filename="../ui/app/RecipeListPage.qml" line="30"/>
+        <source>🍹 Cocktail Automat</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>

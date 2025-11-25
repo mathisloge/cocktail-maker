bf3079a75ff71e6ef0cc36f2b6435e152ece6c84
Mathis Logemann <13556116+mathisloge@users.noreply.github.com>
2025-11-25 20:44:56 +0100

    chore: update translations

--- i18n/cocktail-maker_en.ts
+++ i18n/cocktail-maker_en.ts
@@ -4,13 +4,13 @@
 <context>
     <name>GlassSelectionPage</name>
     <message>
-        <location filename="../ui/app/GlassSelectionPage.qml" line="42"/>
-        <source>Glasgöße wählen</source>
+        <location filename="../ui/app/GlassSelectionPage.qml" line="37"/>
+        <source>Zurück</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/app/GlassSelectionPage.qml" line="49"/>
-        <source>Erkannt: %1</source>
+        <location filename="../ui/app/GlassSelectionPage.qml" line="44"/>
+        <source>Glas wählen</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -25,37 +25,38 @@
 <context>
     <name>MixPage</name>
     <message>
-        <location filename="../ui/app/MixPage.qml" line="21"/>
-        <source>%1 wird zubereitet</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../ui/app/MixPage.qml" line="35"/>
+        <location filename="../ui/app/MixPage.qml" line="63"/>
         <source>Schritte:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/app/MixPage.qml" line="104"/>
+        <location filename="../ui/app/MixPage.qml" line="25"/>
+        <location filename="../ui/app/MixPage.qml" line="44"/>
         <source>Abbrechen</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/app/MixPage.qml" line="113"/>
+        <location filename="../ui/app/MixPage.qml" line="31"/>
+        <source>Wird zubereitet...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/app/MixPage.qml" line="123"/>
         <source>Bitte führe den angezeigten Schritt aus:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/app/MixPage.qml" line="115"/>
+        <location filename="../ui/app/MixPage.qml" line="125"/>
         <source>Fertig ▶</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/app/MixPage.qml" line="126"/>
+        <location filename="../ui/app/MixPage.qml" line="136"/>
         <source>Bitte fülle nach:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/app/MixPage.qml" line="128"/>
+        <location filename="../ui/app/MixPage.qml" line="138"/>
         <source>Aufgefüllt ▶</source>
         <translation type="unfinished"></translation>
     </message>
@@ -63,40 +64,40 @@
 <context>
     <name>RecipeDetailPage</name>
     <message>
-        <location filename="../ui/app/RecipeDetailPage.qml" line="37"/>
+        <location filename="../ui/app/RecipeDetailPage.qml" line="38"/>
         <source>Zurück</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/app/RecipeDetailPage.qml" line="69"/>
+        <location filename="../ui/app/RecipeDetailPage.qml" line="59"/>
+        <source>🍹 Weiter!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/app/RecipeDetailPage.qml" line="94"/>
         <source>Getränk boosten</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/app/RecipeDetailPage.qml" line="81"/>
+        <location filename="../ui/app/RecipeDetailPage.qml" line="106"/>
         <source>mild</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/app/RecipeDetailPage.qml" line="95"/>
+        <location filename="../ui/app/RecipeDetailPage.qml" line="120"/>
         <source>stark</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/app/RecipeDetailPage.qml" line="109"/>
-        <source>Schritte:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../ui/app/RecipeDetailPage.qml" line="141"/>
-        <source>🍹 mixen!</source>
+        <location filename="../ui/app/RecipeDetailPage.qml" line="133"/>
+        <source>Was drin ist</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>RecipeListPage</name>
     <message>
-        <location filename="../ui/app/RecipeListPage.qml" line="30"/>
+        <location filename="../ui/app/RecipeListPage.qml" line="24"/>
         <source>🍹 Cocktail Automat</source>
         <translation type="unfinished"></translation>
     </message>
@@ -104,33 +105,43 @@
 <context>
     <name>cm::db</name>
     <message>
-        <location filename="../ui/db/src/db.cpp" line="15"/>
+        <location filename="../ui/db/src/db.cpp" line="16"/>
         <source>Wasser</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/db/src/db.cpp" line="20"/>
+        <location filename="../ui/db/src/db.cpp" line="21"/>
         <source>Bacardi</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/db/src/db.cpp" line="25"/>
+        <location filename="../ui/db/src/db.cpp" line="26"/>
         <source>Soda Wasser</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/db/src/db.cpp" line="30"/>
+        <location filename="../ui/db/src/db.cpp" line="31"/>
         <source>Limettensaft</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/db/src/db.cpp" line="35"/>
+        <location filename="../ui/db/src/db.cpp" line="36"/>
         <source>Vodka</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/db/src/db.cpp" line="40"/>
+        <location filename="../ui/db/src/db.cpp" line="41"/>
         <source>Orangensaft</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/db/src/db.cpp" line="46"/>
+        <source>Tequila (weiß)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/db/src/db.cpp" line="51"/>
+        <source>Grenadine</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -145,6 +156,7 @@
     </message>
     <message>
         <location filename="../ui/db/src/recipes/screwdriver.hpp" line="25"/>
+        <location filename="../ui/db/src/recipes/tequila_sunrise.hpp" line="26"/>
         <source>2 Eiswürfel</source>
         <translation type="unfinished"></translation>
     </message>
@@ -166,6 +178,16 @@
     <message>
         <location filename="../ui/db/src/recipes/mojito.hpp" line="27"/>
         <source>2 TL Zucker</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/db/src/recipes/tequila_sunrise.hpp" line="17"/>
+        <source>Tequila Sunrise</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/db/src/recipes/tequila_sunrise.hpp" line="18"/>
+        <source>Der Tequila Sunrise ist ein farbenfroher Cocktail aus Tequila, Orangensaft und Grenadine, der mit seinem sanften Farbverlauf und seinem fruchtig-süßen Geschmack Urlaubsstimmung verbreitet.</source>
         <translation type="unfinished"></translation>
     </message>
 </context>

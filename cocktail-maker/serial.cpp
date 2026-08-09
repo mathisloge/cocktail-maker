02b06c4c0fde1e1e971b8f6d711080091f5e71fc
Mathis Logemann <mathisloge@gmail.com>
2026-08-09 09:22:41 +0200

    feat(lib): enhance USB hotplug discovery logging and error handling

--- cocktail-maker/serial.cpp
+++ cocktail-maker/serial.cpp
@@ -3,6 +3,7 @@ module;
 #include <boost/cobalt/generator.hpp>
 #include <boost/cobalt/op.hpp>
 #include <boost/cobalt/this_coro.hpp>
+#include <spdlog/spdlog.h>
 
 #ifdef __linux__
 #include <boost/asio/posix/stream_descriptor.hpp>
@@ -13,6 +14,7 @@ module;
 
 module cm:serial_impl;
 import std;
+import cm.core;
 import :serial;
 
 namespace cm {
@@ -49,23 +51,28 @@ using udev_device_ptr = std::unique_ptr<udev_device, UdevDeviceDeleter>;
 
 boost::cobalt::generator<std::shared_ptr<IPod>> SerialPodDiscovery::discover()
 {
+    auto logger = cm::log::create_or_get("serial");
+
 #ifndef __linux__
     // On non-Linux systems, the generator exits immediately.
+    SPDLOG_LOGGER_ERROR(logger, "USB hotplug discovery is only implemented for Linux. Aborting discovery.");
     throw SerialInitializationException{};
 #else
 
     auto exec = co_await boost::cobalt::this_coro::executor;
 
-    constexpr std::string TARGET_VID = "1234"; // TODO: Replace with Vendor ID
-    constexpr std::string TARGET_PID = "abcd"; // TODO: Replace with Product ID
+    constexpr std::string TARGET_VID = "2fe3"; // TODO: Replace with Vendor ID
+    constexpr std::string TARGET_PID = "0001"; // TODO: Replace with Product ID
 
     udev_ptr udev_ctx{udev_new()};
     if (!udev_ctx) {
+        SPDLOG_LOGGER_ERROR(logger, "Could not create udev context.");
         throw SerialInitializationException();
     }
 
     udev_monitor_ptr mon{udev_monitor_new_from_netlink(udev_ctx.get(), "udev")};
     if (!mon) {
+        SPDLOG_LOGGER_ERROR(logger, "Could not create udev monitor.");
         throw SerialMonitorException();
     }
 
@@ -75,11 +82,14 @@ boost::cobalt::generator<std::shared_ptr<IPod>> SerialPodDiscovery::discover()
     const int fd = udev_monitor_get_fd(mon.get());
     boost::asio::posix::stream_descriptor monitor_descriptor(exec, fd);
 
+    SPDLOG_LOGGER_INFO(logger, "Watching udev for tty devices with VID '{}' and PID '{}'.", TARGET_VID, TARGET_PID);
+
     for (;;) {
         co_await monitor_descriptor.async_wait(boost::asio::posix::descriptor_base::wait_read, boost::cobalt::use_op);
 
         udev_device_ptr dev{udev_monitor_receive_device(mon.get())};
         if (!dev) {
+            SPDLOG_LOGGER_TRACE(logger, "Received empty udev device event, skipping.");
             continue;
         }
 
@@ -93,18 +103,26 @@ boost::cobalt::generator<std::shared_ptr<IPod>> SerialPodDiscovery::discover()
 
             if (vendor_id && model_id && devnode) {
                 if (TARGET_VID == vendor_id && TARGET_PID == model_id) {
+                    SPDLOG_LOGGER_INFO(logger, "Matching serial pod found at '{}' (VID '{}', PID '{}').", devnode, vendor_id, model_id);
                     try {
                         boost::asio::serial_port serial_port(exec, devnode);
 
                         serial_port.set_option(boost::asio::serial_port_base::baud_rate(115200));
                         serial_port.set_option(boost::asio::serial_port_base::character_size(8));
 
+                        SPDLOG_LOGGER_DEBUG(logger, "Opened serial port '{}'.", devnode);
+
                         co_yield std::make_shared<Pod>(
                             std::make_unique<SocketIoStream<boost::asio::serial_port>>(std::move(serial_port)));
                     }
                     catch (const std::exception& e) {
+                        SPDLOG_LOGGER_ERROR(logger, "Failed to open serial port '{}': {}", devnode, e.what());
                         throw SerialPortOpenException(devnode, e.what());
                     }
+                }
+                else {
+                    SPDLOG_LOGGER_TRACE(
+                        logger, "Ignoring tty device '{}' with non-matching VID '{}' / PID '{}'.", devnode, vendor_id, model_id);
                 }
             }
         }
